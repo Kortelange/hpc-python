2d9d91cb2d79b89e429c99e250649ddfa9d32f47
Jakob Lange <jakob.lange@bring.com>
2022-12-30 13:07:41 +0100

    add game of life solution

---
+++ numpy/game-of-life/game_of_life.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class GOL:
+    #square game of life
+    def __init__(self, size=4, board=None):
+        if board is None:
+            self.board = np.random.randint(0, 2, size=(size, size))
+            self.size = size
+        else:
+            self.board = board
+            self.size = board.shape[0]
+        self.reset_neighbors()
+
+    def reset_neighbors(self):
+        self.neighbors = np.zeros_like(self.board)
+
+    def get_right_neighbors(self, column):
+        right_neighbors = self.board[:, column + 1].copy()
+        right_neighbors[:-1] += self.board[1:, column + 1]
+        right_neighbors[1:] += self.board[:-1, column + 1]
+        return right_neighbors
+
+    def get_left_neighbors(self, column):
+        left_neighbors = self.board[:, column - 1].copy()
+        left_neighbors[:-1] += self.board[1:, column - 1]
+        left_neighbors[1:] += self.board[: -1, column - 1]
+        return left_neighbors
+
+    def get_upper_neighbors(self, row):
+        return self.board[row - 1, :].copy()
+
+    def get_lower_neighbors(self, row):
+        return self.board[row + 1, :].copy()
+
+    def get_neighbours(self):
+        self.reset_neighbors()
+        neighbors = self.neighbors
+        # get neighbors of first row, column
+        neighbors[0, :] += self.get_lower_neighbors(0)
+        neighbors[:, 0] += self.get_right_neighbors(0)
+        # get neighbors of last row, column
+        neighbors[-1, :] += self.get_upper_neighbors(-1)
+        neighbors[:, -1] += self.get_left_neighbors(-1)
+
+        # fill in last neighbors
+        for i in range(1, self.size - 1):
+            neighbors[i, :] += self.get_lower_neighbors(i)
+            neighbors[i, :] += self.get_upper_neighbors(i)
+            neighbors[:, i] += self.get_left_neighbors(i)
+            neighbors[:, i] += self.get_right_neighbors(i)
+
+    def update(self):
+        self.get_neighbours()
+        # handle zeroes
+        zeroes = self.board == 0
+        self.board[zeroes] += (self.neighbors[zeroes] == 3) * 1
+        # handle ones
+        ones = self.board == 1
+        self.board[ones] -= (self.neighbors[ones] < 2) * 1
+        self.board[ones] -= (self.neighbors[ones] > 3) * 1
+
+    def play(self, maxiterations=500):
+        for i in range(maxiterations):
+            previous_board = self.board.copy()
+            self.update()
+            if (self.board == previous_board).all():
+                break
+        if i == maxiterations - 1:
+            print('Solution is not complete')
+
+    # def animate
+
+
+board = np.zeros((32, 32))
+board[15, :] = 1
+board[:, 15] = 1
+gol = GOL(size=32)
+plt.subplot(1,2,1)
+plt.imshow(gol.board)
+
+gol.play()
+plt.subplot(1,2,2)
+plt.imshow(gol.board)
+plt.show()
