0cb420e3af25cc1c38f841ee07cf71ec10181490
Jakob Lange <jakob.lange@bring.com>
2022-12-30 09:28:45 +0100

    add broadcast_rotation solution

---
+++ numpy/broadcast-rotation/broadcast_rotation.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+points = np.loadtxt('points_circle.dat')
+
+theta = 90
+
+# We flip the matrix and do the transposed multiplication
+M = np.array([[np.cos(theta), np.sin(theta)],
+              [-np.sin(theta), np.cos(theta)]])
+
+plt.plot(points[:, 0], points[:, 1],  'o')
+rotated_points = points.dot(M)
+plt.plot(rotated_points[:, 0], rotated_points[:, 1], 'o')
+plt.show()
